d5ad32966c41f178ebb3770dd531b8a54d31baea
Mattheww31 <49414872+Mattheww31@users.noreply.github.com>
2025-11-08 09:17:33 -0500

    -basic ui add

---
+++ Assets/Scripts/Button Manager.cs
@@ -0,0 +1,67 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    enum CurrentSceneName
+    {
+        Menu,
+        MainGame1,
+        MainGame2,
+        MainGame3,
+None,
+
+    }
+    CurrentSceneName currentScene = CurrentSceneName.None;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentScene == CurrentSceneName.Menu)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+        if (currentScene == CurrentSceneName.MainGame1)
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+        if (currentScene == CurrentSceneName.MainGame2)
+        {
+           SceneManager.LoadScene("Level 2");
+        }
+          if (currentScene == CurrentSceneName.MainGame3)
+        {
+             SceneManager.LoadScene("Level 3");
+        }
+
+    }
+    void OnMouseDown()
+    {
+        if (gameObject.name.Contains("1"))
+        {
+            currentScene = CurrentSceneName.MainGame1;
+        }
+         if (gameObject.name.Contains("2"))
+        {
+            currentScene = CurrentSceneName.MainGame2;
+        }
+         if (gameObject.name.Contains("3"))
+        {
+            currentScene = CurrentSceneName.MainGame3;
+        }
+        if (gameObject.name.Contains("Menu"))
+        {
+            currentScene = CurrentSceneName.Menu;
+        }
+         if (gameObject.name.Contains("Exit"))
+        {
+            Application.Quit();
+        }
+    }
+}
