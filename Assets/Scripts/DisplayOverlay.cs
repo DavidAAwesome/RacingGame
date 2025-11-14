64d827a663f79e31d54134feed99b33e9ef55acb
Mattheww31 <49414872+Mattheww31@users.noreply.github.com>
2025-11-14 04:52:19 -0500

    -ready set go

--- Assets/Scripts/DisplayOverlay.cs
+++ Assets/Scripts/DisplayOverlay.cs
@@ -8,6 +8,10 @@ public class DisplayOverlay : MonoBehaviour
     public static GameObject Overlay1_Global;
     public GameObject Overlay2;
     public GameObject Overlay3;
+    public GameObject Ready;
+     public GameObject Set;
+      public GameObject Go;
+      float time = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +21,9 @@ public class DisplayOverlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ReadySetGo_Start();
+time+=Time.deltaTime;
+
         if ((!gameObject.name.Contains("Win") && (!gameObject.name.Contains("Lose")) && Input.GetKey(KeyCode.Escape)))
         {
             Overlay.SetActive(true);
@@ -29,5 +36,33 @@ public class DisplayOverlay : MonoBehaviour
         {
              Overlay3.SetActive(true);
         }
+    }
+    void ReadySetGo_Start(){
+        Ready_Func();
+        if(time>2){
+            Set_Func();
+        }
+         if(time>4){
+            Go_Func();
+        }
+         if(time>6){
+            Go.SetActive(false);
+        }
+    }
+    void Ready_Func(){
+        Ready.SetActive(true);
+        Set.SetActive(false);
+       Go.SetActive(false);
+    }
+     void Set_Func(){
+         Set.SetActive(true);
+Ready.SetActive(false);
+Go.SetActive(false);
+    }
+     void Go_Func(){
+
+Go.SetActive(true);
+Set.SetActive(false);
+Ready.SetActive(false);
     }
 }
